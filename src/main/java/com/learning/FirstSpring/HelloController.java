515a8c87ff622970ba6af0b2675617013d5b6298
Shahid khan <professionalshahid0@gmail.com>
2025-12-14 11:58:19 +0530

    get and post methods

---
+++ src/main/java/com/learning/FirstSpring/HelloController.java
@@ -0,0 +1,18 @@
+package com.learning.FirstSpring;
+
+import org.springframework.web.bind.annotation.*;
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public HelloResponse hello(){
+        return new HelloResponse("Hello World");
+    }
+    @GetMapping("/hello/{name}")
+    public HelloResponse helloParam(@PathVariable String name){
+        return new HelloResponse("Hello World by "+ name);
+    }
+    @PostMapping("/hello")
+    public HelloResponse hello2(@RequestBody String name){
+        return new HelloResponse("Hello "+ name);
+    }
+}
